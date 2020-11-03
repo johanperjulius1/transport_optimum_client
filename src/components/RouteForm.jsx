0be0cd0aa3c0709791f9dbadff03b5accc3a4cd8
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-11-03 18:20:27 +0100

    Adding headers, creating routeInformation start address, end address, distance and duration to Message.Header

--- src/components/RouteForm.jsx
+++ src/components/RouteForm.jsx
@@ -3,32 +3,34 @@ import { Button, Form, Container, Message } from "semantic-ui-react";
 import axios from "axios";
 
 const RouteForm = () => {
-  const [confirmationMessage, setConfirmationMessage] = useState([])
+  const [routeInformation, setRouteInformation] = useState();
   const postRoute = async (event) => {
     event.preventDefault();
-    debugger
     try {
-      debugger
       const origin = event.target.origin.value;
       const destination = event.target.destination.value;
 
       const apiKey = process.env.REACT_APP_MAPSDIRECTIONS_API_KEY;
 
       const response = await axios.post(
-        `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}`
+        `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}`,
+        { headers: "Access-Control-Allow-Origin" }
       );
-      setMessage[response.data.routes[0].legs[0].start_address]
-      debugger
-    } catch(error) {
-      debugger
-    }
-  }
+      setRouteInformation(response.data.routes[0].legs[0]);
+    } catch (error) {}
+  };
 
   return (
     <Container>
-      {confirmationMessage && (
-        <Message data-cy='confirmation-message'>
-          <Message.header>{confirmationMessage}. Your route</Message.header>
+      {routeInformation && (
+        <Message data-cy="route-information">
+          <Message.Header data-cy="confirmation-message">
+            Your route
+            <br /> Starting point: {routeInformation.start_address}
+            Destination: {routeInformation.end_address}
+            <br /> Distance: {routeInformation.distance.text}
+            <br /> Duration: {routeInformation.duration.text}
+          </Message.Header>
         </Message>
       )}
       <Form data-cy="route-form" onSubmit={(event) => postRoute(event)}>
@@ -52,8 +54,8 @@ const RouteForm = () => {
           data-cy="submit-route"
           id="submit-route"
           content="Submit Your Route"
-        >
-        </Button>
+          color="green"
+        ></Button>
       </Form>
     </Container>
   );
