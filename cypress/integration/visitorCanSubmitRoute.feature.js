0c144506d4f63f1ec3895071938ed7b59c10803b
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-11-03 10:42:34 +0100

    Writes first integration test.

---
+++ cypress/integration/visitorCanSubmitRoute.feature.js
@@ -0,0 +1,34 @@
+describe("visitor can submit route", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: "http://localhost:",
+      response: "",
+    });
+    cy.visit("/");
+  });
+
+  it("visitor can see route form", () => {
+    cy.get("[data-cy='route-form']").should("be.visible");
+  });
+
+  it("visitor can fill in route form", () => {
+    cy.get("[data-cy='route-form']").within(() => {
+      cy.get("[data-cy='origin']").type("Stockholm");
+      cy.get("[data-cy='destination']").type("Orebro");
+      cy.get("[data-cy='submit-route']").click();
+    });
+  });
+
+  context("successfully created route", () => {
+    it("displays the correct route information", () => {
+      cy.get("[data-cy='route-information']").within(() => {
+        cy.get("[data-cy='origin']").should("contain", "Stockholm");
+        cy.get("[data-cy='destination']").should("contain", "Orebro");
+        cy.get("[data-cy='route-distance']").should("contain", "193 km");
+        cy.get("[data-cy='route-time']").should("contain", "2h 45min")
+      });
+    });
+  });
+});
