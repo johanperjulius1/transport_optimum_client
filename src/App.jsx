0f4b2fdd33ead04384fa2599ca1b3d517506c639
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-11-08 13:24:02 +0100

    Installing react-google-maps-api, creating Map component, adding current posistion marker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import RouteForm from "./components/RouteForm";
+import Map from './components/Map'
 
 const App = () => {
   return (
+    <>
     <RouteForm />
+    <Map />
+    </>
   )
 };
 
---
+++ src/components/Map.jsx
@@ -0,0 +1,48 @@
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useState, useEffect } from "react";
+
+const Map = () => {
+  const [currentPosition, setCurrentPosition] = useState({});
+  const apiKey = process.env.REACT_APP_MAPSDIRECTIONS_API_KEY;
+  const success = (position) => {
+    const currentPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    setCurrentPosition(currentPosition);
+  };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  });
+  const onMarkerDragEnd = (e) => {
+    const lat = e.latLng.lat();
+    const lng = e.latLng.lng();
+    setCurrentPosition({ lat, lng });
+  };
+  const mapStyles = {
+    height: "80vh",
+    width: "100%",
+  };
+
+  return (
+    <div className="map" data-cy="map">
+      <LoadScript googleMapsApiKey={apiKey}>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={currentPosition}
+        >
+          {currentPosition.lat ? (
+            <Marker
+              position={currentPosition}
+              onDragEnd={(e) => onMarkerDragEnd(e)}
+              draggable={true}
+            />
+          ) : null}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
+
+export default Map;
